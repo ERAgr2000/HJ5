35631cbc22e228c521728ac7f2ad98b1674a1401
Elder <53918860+ERAgr2000@users.noreply.github.com>
2021-03-14 12:30:00 -0600

    Version final
    Version final del programa.

--- simulacion.py
+++ simulacion.py
@@ -2,6 +2,7 @@ import simpy
 import queue
 import random 
 import time
+import statistics as stats
 
 #clase que representa un proceso del Sistema operativo
 class Proceso:
@@ -13,15 +14,16 @@ class Proceso:
 
 class SistemaOperativo:
     
-    def __init__(self,env, procesos_tot, cpu_instrucciones):
+    def __init__(self,env, procesos_tot, cpu_instrucciones,cpus, memRAM):
         #variables para almacenamineto de parametros del SO
         self.num_procesos = procesos_tot
         self.cpu_lim = cpu_instrucciones
         self.procesos_terminados = 0
         self.enviroment = env
-        self.RAM = simpy.Container(env, init=100, capacity=100)
-        self.CPU = simpy.Resource(env, capacity=1)
+        self.RAM = simpy.Container(env, init=memRAM, capacity=memRAM)
+        self.CPU = simpy.Resource(env, capacity=cpus)
         self.distribution = []
+        self.cpus = cpus
 
     #Crea los procesos y los asigna en un array
     #siendo "a" el numero de procesos de la simulacion
@@ -51,26 +53,26 @@ class SistemaOperativo:
             CPU_list = []
             
             while(self.procesos_terminados < self.num_procesos):
-            
-                if self.CPU.count > 0:
-                    self.CPU.release(self.CPU.users[0])
-                    procesado = CPU_list[0]
-                    procesado.instrucciones =- 3
+                for i in range(self.cpus):
+                    if self.CPU.count > 0:
+                        self.CPU.release(self.CPU.users[0])
+                        procesado = CPU_list[0]
+                        procesado.instrucciones =- self.cpu_lim
 
-                    if procesado.instrucciones > 0:
-                        wait = random.randint(1,2)
-                        
-                        if wait == 1:
-                            ready.append(procesado)
-                            self.RAM.get(procesado.memoria)
+                        if procesado.instrucciones > 0:
+                            wait = random.randint(1,2)
+                            
+                            if wait == 1:
+                                ready.append(procesado)
+                                self.RAM.get(procesado.memoria)
+                            else:
+                                waiting.append(procesado)
                         else:
-                            waiting.append(procesado)
-                    else:
-                        self.procesos_terminados += 1
-                        tiempos.append(time.time() - procesado.tiempo)
+                            self.procesos_terminados += 1
+                            tiempos.append(time.time() - procesado.tiempo)
                         
 
-                    CPU_list.pop(0)
+                        CPU_list.pop(0)
                 
                 #para saber si las colas estan vacias
                 if len(waiting) != 0:
@@ -114,26 +116,30 @@ def average(lista):
     return promedio
     
 #Parametros de la simulacion
+memRAM = 100
+cpus = 1
 cpu_instrucciones = 3
+intervalo = 1
 num_procesos = [25,50,100,150,200]
-intervalo = 10
 min_instrucciones = 1
 max_instrucciones = 10
 min_RAM = 1
 max_RAM = 10
-file1 = open('Mem100Int10CPU1Inst3.csv', 'w') 
+file1 = open('Mem'+str(memRAM)+'Int'+str(intervalo)+'CPU'+str(cpus)+'Inst'+str(cpu_instrucciones)+'.csv', 'w') 
 
 #semilla para la creacion pseudo random
 RANDOM_SEED = 25
-random.seed(RANDOM_SEED)
 
 datas = "Procesos, Tiempo"
 for i in range(len(num_procesos)):
+    random.seed(RANDOM_SEED)
     env = simpy.Environment()
-    so = SistemaOperativo(env, num_procesos[i], cpu_instrucciones)
+    so = SistemaOperativo(env, num_procesos[i], cpu_instrucciones, cpus, memRAM)
     so.creacion(intervalo)
     lista_tiempos = so.simulacion(min_instrucciones, max_instrucciones, min_RAM, max_RAM)
     x=average(lista_tiempos)
+    est=stats.pstdev(lista_tiempos)
+    print("El promedio para " +str(num_procesos[i])+" procesos es de " + str(x) +" y la desviacion estandar es de " + str(est) )
     datas += "\n" + str(num_procesos[i])+"," + str(x)
     
 file1.write(datas)
